375c46e34925423f9dacc037d7159643f59ea9aa
Domino731 <d.orzechowski18@gmail.com>
2023-03-21 20:49:07 +0100

    rest view & response

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,12 +4,18 @@ from django.http import JsonResponse, HttpResponse
 from django.forms.models import model_to_dict
 from products.models import Product
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+
+@api_view(['GET'])
 def api_home(request):
     model_data = Product.objects.all().order_by('?').first()
     data = {}
+    if request.method != 'POST':
+        return Response({'message': 'GET not allowed'}, status=405)
     if model_data:
         data = model_to_dict(model_data, fields=['title', 'id', 'content', 'price'])
-    return JsonResponse(data)
+    return Response(data)
     #     json_data_str = json.dumps(data)
     # return HttpResponse(json_data_str, headers={'content-type': 'application/json'})
