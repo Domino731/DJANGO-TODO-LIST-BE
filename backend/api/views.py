eab16768681ecbded0c27f617bf6810f99b8697a
Domino731 <d.orzechowski18@gmail.com>
2023-03-22 17:08:47 +0100

    get serializer for product

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,21 +1,21 @@
 import json
-
 from django.http import JsonResponse, HttpResponse
 from django.forms.models import model_to_dict
 from products.models import Product
-
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
 
 
 @api_view(['GET'])
 def api_home(request):
-    model_data = Product.objects.all().order_by('?').first()
+    instance = Product.objects.all().order_by('?').first()
     data = {}
-    if request.method != 'POST':
-        return Response({'message': 'GET not allowed'}, status=405)
-    if model_data:
-        data = model_to_dict(model_data, fields=['title', 'id', 'content', 'price'])
+    if request.method != 'GET':
+        return Response({'message': 'GET is only allowed'}, status=405)
+    if instance:
+        # data = model_to_dict(instance, fields=['title', 'id', 'content', 'price', 'sale_price'])
+        data = ProductSerializer(instance).data
     return Response(data)
     #     json_data_str = json.dumps(data)
     # return HttpResponse(json_data_str, headers={'content-type': 'application/json'})
--- backend/products/models.py
+++ backend/products/models.py
@@ -6,3 +6,10 @@ class Product(models.Model):
     title = models.CharField(max_length=100, default="FOO")
     content = models.TextField(blank=True, null=True, default="FOO")
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+
+    @property
+    def sale_price(self):
+        return "%.2f" % (float(self.price) * 0.8)
+
+    def get_discount(self):
+        return '199'
