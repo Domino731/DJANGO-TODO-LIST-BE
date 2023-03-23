6ed2378e0db174163b3a144241912b1d4b41dfcf
Domino731 <d.orzechowski18@gmail.com>
2023-03-23 17:47:54 +0100

    api_home clearn

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,3 @@
-import json
-from django.http import JsonResponse, HttpResponse
-from django.forms.models import model_to_dict
 from products.models import Product
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -14,8 +11,8 @@ def api_home(request):
     if request.method != 'GET':
         return Response({'message': 'GET is only allowed'}, status=405)
     if instance:
-        # data = model_to_dict(instance, fields=['title', 'id', 'content', 'price', 'sale_price'])
         data = ProductSerializer(instance).data
     return Response(data)
+    # data = model_to_dict(instance, fields=['title', 'id', 'content', 'price', 'sale_price'])
     #     json_data_str = json.dumps(data)
     # return HttpResponse(json_data_str, headers={'content-type': 'application/json'})
