6d6a99082a70d60689460a028389742ddd446f41
Domino731 <d.orzechowski18@gmail.com>
2023-03-30 19:46:57 +0200

    adding new product

--- backend/products/models.py
+++ backend/products/models.py
@@ -6,13 +6,3 @@ class Product(models.Model):
     title = models.CharField(max_length=100, default="UNKNOWN")
     content = models.TextField(blank=True, null=True, default="UNKNOWN")
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
-
-    @property
-    def sale_price(self):
-        return "%.2f" % (float(self.price) * 0.8)
-
-    def get_discount(self):
-        return "DISCOUNT"
-
-    def get_test(self):
-        return "TEST"
