46544259f242f87220365d5f73e3ec92d52b2812
Domino731 <d.orzechowski18@gmail.com>
2023-03-31 18:04:35 +0200

    new field for product

--- backend/products/models.py
+++ backend/products/models.py
@@ -6,3 +6,4 @@ class Product(models.Model):
     title = models.CharField(max_length=100, default="UNKNOWN")
     content = models.TextField(blank=True, null=True, default="UNKNOWN")
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+    brand = models.CharField(max_length=100, default="UNKNOWN")
